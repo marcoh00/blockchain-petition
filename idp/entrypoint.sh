e38ddb59e3e08d11c0c8cfa4f0f3a3730ecbd14d
Marco Hünseler <marco.huenseler@h-brs.de>
2022-04-11 12:22:12 +0200

    [idp] create dockerfile for easy deployments

---
+++ idp/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Usage: entrypoint network dbfile nodeargs...
+
+set -e -x
+
+NETWORK=${1:-goerli}
+DBFILE=${2:-/home/idp/database.db}
+
+sed -i 's/exports.DEFAULT_NETWORK = exports.NETWORKS\\./exports.DEFAULT_NETWORK = exports.NETWORKS.'"${NETWORK}"';/g' dist/shared/addr.js
+sed -i 's#exports.DBFILE = ".*#exports.DBFILE = "'"${DBFILE}"'";#g' dist/shared/addr.js
+
+exec /usr/bin/node "${@:3}"
