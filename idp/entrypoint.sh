02ea5bd8d72eedc470b11f6b99f32bd5f3044d4d
Marco Hünseler <marco.huenseler@h-brs.de>
2022-04-13 13:12:09 +0200

    [idp] fix entrypoint

--- idp/entrypoint.sh
+++ idp/entrypoint.sh
@@ -7,7 +7,7 @@ set -e -x
 NETWORK=${1:-goerli}
 DBFILE=${2:-/home/idp/database.db}
 
-sed -i 's/exports.DEFAULT_NETWORK = exports.NETWORKS\\./exports.DEFAULT_NETWORK = exports.NETWORKS.'"${NETWORK}"';/g' dist/shared/addr.js
+sed -i 's/exports.DEFAULT_NETWORK = exports.NETWORKS\..*;/exports.DEFAULT_NETWORK = exports.NETWORKS.'"${NETWORK}"';/g' dist/shared/addr.js
 sed -i 's#exports.DBFILE = ".*#exports.DBFILE = "'"${DBFILE}"'";#g' dist/shared/addr.js
 
 exec /usr/bin/node "${@:3}"
