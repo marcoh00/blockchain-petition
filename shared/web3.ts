a05eb5a0ed978c5978838024b3763538aa30f660
Marco Hünseler <marco.huenseler@h-brs.de>
2022-09-12 10:11:20 +0200

    shared: hex to string fallback

--- shared/web3.ts
+++ shared/web3.ts
@@ -111,9 +111,16 @@ export class EthereumConnector {
         const addr_list: string[] = await this.registrycontract.methods.petitions().call();
         for(const addr of addr_list) {
             const contract = new this.api.eth.Contract((PetitionContract.abi as any), addr);
+            const name = await contract.methods.name().call();
+            let name_decoded = "";
+            try {
+                name_decoded = hexToUtf8(name);
+            } catch(e) {
+                name_decoded = EthereumConnector.hexToUtf8Fallback(name);
+            }
             const info: IPetition = {
                 address: addr,
-                name: hexToUtf8(await contract.methods.name().call()),
+                name: name_decoded,
                 description: await contract.methods.description().call(),
                 id: new Uint8Array(hexToBytes(await contract.methods.id().call())),
                 period: Number.parseInt(await contract.methods.period().call()),
@@ -138,4 +145,21 @@ export class EthereumConnector {
         const tx = await this.registrycontract.methods.createPetition(name_b32, description, period).send({ from: this.account });
         return tx;
     }
+
+    static ASCII_TABLE: ITable = {"20": " ", "21": "!", "22": "\"", "23": "#", "24": "$", "25": "%", "26": "&", "27": "'", "28": "(", "29": ")", "2a": "*", "2b": "+", "2c": ",", "2d": "-", "2e": ".", "2f": "/", "30": "0", "31": "1", "32": "2", "33": "3", "34": "4", "35": "5", "36": "6", "37": "7", "38": "8", "39": "9", "3a": ":", "3b": ";", "3c": "<", "3d": "=", "3e": ">", "3f": "?", "40": "@", "41": "A", "42": "B", "43": "C", "44": "D", "45": "E", "46": "F", "47": "G", "48": "H", "49": "I", "4a": "J", "4b": "K", "4c": "L", "4d": "M", "4e": "N", "4f": "O", "50": "P", "51": "Q", "52": "R", "53": "S", "54": "T", "55": "U", "56": "V", "57": "W", "58": "X", "59": "Y", "5a": "Z", "5b": "[", "5c": "\\", "5d": "]", "5e": "^", "5f": "_", "60": "`", "61": "a", "62": "b", "63": "c", "64": "d", "65": "e", "66": "f", "67": "g", "68": "h", "69": "i", "6a": "j", "6b": "k", "6c": "l", "6d": "m", "6e": "n", "6f": "o", "70": "p", "71": "q", "72": "r", "73": "s", "74": "t", "75": "u", "76": "v", "77": "w", "78": "x", "79": "y", "7a": "z", "7b": "{", "7c": "|", "7d": "}", "7e": "~"};
+    static hexToUtf8Fallback(hexstring: string) {
+        if(hexstring.length % 2 !== 0) throw Error("Benötige ganze Bytes");
+        let output = "";
+        for(let i = 0; i < hexstring.length; i += 2) {
+            const byte = `${hexstring.charAt(i).toLowerCase()}${hexstring.charAt(i + 1).toLowerCase()}`;
+            if(byte === "0x" || byte === "00") continue;
+            if(EthereumConnector.ASCII_TABLE.hasOwnProperty(byte)) output = `${output}${EthereumConnector.ASCII_TABLE[byte]}`
+            else { console.log("Invalid character", byte) }
+        }
+        return output;
+    }
+}
+
+interface ITable {
+    [hexbyte: string]: string
 }
