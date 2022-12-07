360bb1e5c7f83874cfb7338dd2be6550dd2bfe4c
Marco Hünseler <marco.huenseler@h-brs.de>
2022-12-07 10:33:09 +0100

    [idp] make entrypoint agnostic to the location of the node executable

--- idp/entrypoint.sh
+++ idp/entrypoint.sh
@@ -10,4 +10,4 @@ DBFILE=${2:-/home/idp/database.db}
 sed -i 's/exports.DEFAULT_NETWORK = exports.NETWORKS\..*;/exports.DEFAULT_NETWORK = exports.NETWORKS.'"${NETWORK}"';/g' dist/shared/addr.js
 sed -i 's#exports.DBFILE = ".*#exports.DBFILE = "'"${DBFILE}"'";#g' dist/shared/addr.js
 
-exec /usr/bin/node "${@:3}"
+exec node "${@:3}"
