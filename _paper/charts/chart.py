17562393c96aad91f96a6cb023c146f3264083eb
Marco Hünseler <marco.huenseler@h-brs.de>
2025-04-14 16:46:34 +0200

    recent gas prices and exchange rates

--- _paper/charts/chart.py
+++ _paper/charts/chart.py
@@ -19,33 +19,40 @@ def avg_gas_price(filename, filter):
             parts = line.split(',')
             parts[0] = parts[0][1:-1]
             parts[1] = int(parts[1][1:-1])
-            parts[2] = int(parts[2][1:-2])
+            if "." in parts[2]:
+                parts[2] = int(parts[2][1:-2].split(".")[0])
+            else:
+                parts[2] = int(parts[2][1:-2])
             if filter(parts):
                 wei.append(parts[2])
         return wei
 
-bar_width = 0.15
-fig, ax = plt.subplots(layout='constrained') 
+bar_width = 0.3
+fig, axs = plt.subplots(1, 2, layout='constrained') 
 
 create = [775454, 855820, 657387, 772340, 772494]
 sign = [90395, 525238, 330368, 2165940, 137234]
 approve = [50002, 79306, 129891, 0, 0]
 
-# 4004,24 2024-12-16
-eth_price = 4000
+# 1637.48 2024-04-13 https://www.kraken.com/prices/ethereum
+eth_price = 1637
 
 # Block 21411883
 #eth_gas = 10.909095097 + 0.1
 
-eth_gas_2024 = avg_gas_price("export-AvgGasPrice.csv", lambda parts: parts[0].split("/")[2] == "2024")
+eth_gas_2024 = avg_gas_price("export-AvgGasPrice.csv", lambda parts: parts[0].split("/")[2] == "2024" or parts[0].split("/")[2] == "2025")
 
 # Block 129357473
 #op_gas = 0.000058466 + 0.0005
-op_gas_2024 = avg_gas_price("export-AvgGasPrice-op.csv", lambda parts: parts[0].split("/")[2] == "2024")
+op_gas_2024 = avg_gas_price("export-AvgGasPrice-op.csv", lambda parts: parts[0].split("/")[2] == "2024" or parts[0].split("/")[2] == "2025")
+
+eth_price_2024 = avg_gas_price("export-EtherPrice.csv", lambda parts: parts[0].split("/")[2] == "2024" or parts[0].split("/")[2] == "2025")
 
 eth_gas = np.median(eth_gas_2024)
 op_gas = np.median(op_gas_2024)
+eth_price = np.median(eth_price_2024)
 
+print(f"Avg ETH price: {eth_price}")
 print(f"Median Gas Prices. ETH: {eth_gas:.2f}, OP: {op_gas:.2f}")
 
 gwei = 10**-9
@@ -80,40 +87,58 @@ pos4 = [x + bar_width for x in pos3]
 pos5 = [x + bar_width for x in pos4]
 pos6 = [x + bar_width for x in pos5]
 
-b1 = ax.bar(pos1, eth_create, width=bar_width, label='ETH Create', color=c1, log=True)
+ax = axs[0]
+b1 = ax.bar(pos1, eth_create, width=bar_width, label='Ethereum Create', color=c1)#, log=True)
 for b in b1:
-        ax.text(b.get_x() + b.get_width()/2., 1+b.get_height(), f"${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 10}, rotation=90)
+        ax.text(b.get_x() + b.get_width()/2., 1+b.get_height(), f"${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 8}, rotation=90)
 #for x, y in zip(pos1, eth_sign):
     #ax.text(x + 0.1, y + 1, f"${y:.2f}", fontdict={"size": 10})
     #tp = TextPath((x + 0.1,y + 1), f"${y:.2f}", size=0.4)
     #ax.add_patch(PathPatch(tp, color="black"))
 
-b2 = ax.bar(pos2, eth_sign, width=bar_width, label='ETH Sign', color=c4)
+b2 = ax.bar(pos2, eth_sign, width=bar_width, label='Ethereum Sign', color=c4)
 for b in b2:
         rotation = 0 if b.get_height() > 100 else 90
-        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 10}, rotation=rotation)
+        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 8}, rotation=rotation)
 
-b3 = ax.bar(pos3, eth_approve, width=bar_width, label='ETH Approve', color=c8)
+b3 = ax.bar(pos3, eth_approve, width=bar_width, label='Ethereum Approve', color=c8)
 for b in b3:
-        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 10}, rotation=90)
+        if b.get_height() == 0:
+              continue
+        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 8}, rotation=90)
 
-b4 = ax.bar(pos4, op_create, width=bar_width, label='OP Create', color=c1, edgecolor="white", hatch="/")
+ax = axs[1]
+
+b4 = ax.bar(pos1, op_create, width=bar_width, label='Optimism Create', color=c1, edgecolor="white", hatch="/")
 for b in b4:
-        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 10}, rotation=90)
+        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 8}, rotation=90)
 
 
-b5 = ax.bar(pos5, op_sign, width=bar_width, label='OP Sign', color=c4, edgecolor="white", hatch="/")
+b5 = ax.bar(pos2, op_sign, width=bar_width, label='Optimism Sign', color=c4, edgecolor="white", hatch="/")
 for b in b5:
-        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 10}, rotation=90)
+        rotation = 0 if b.get_height() > 0.8 else 90
+        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 8}, rotation=rotation)
 
-b6 = ax.bar(pos6, op_approve, width=bar_width, label='OP Approve', color=c8, edgecolor="white", hatch="/")
+b6 = ax.bar(pos3, op_approve, width=bar_width, label='Optimism Approve', color=c8, edgecolor="white", hatch="/")
 for b in b6:
-        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 10}, rotation=90)
+        if b.get_height() == 0:
+              continue
+        ax.text(b.get_x() + b.get_width()/2., b.get_height(), f" ${b.get_height():.2f}", ha='center', va='bottom', fontdict={"size": 8}, rotation=90)
 
 #plt.yscale('log')
+ax = axs[0]
 
-ax.set_ylabel("Estimated price in USD (log)")
-ax.set_xticks([r + bar_width + (1.5 * bar_width) for r in range(len(create))], ["Eth. Addr.", "MT Membership", "Semaphore", "Pseudonym. Sig.\nsecp256k1", "Pseudonym. Sig.\nalt_bn128"])
+for i in range(0, 2):
+      axs[i].set_ylabel("Estimated price in USD")
+      axs[i].set_xticks([r + bar_width + (0 * bar_width) for r in range(len(create))], ["III-C", "III-D", "III-E", "III-F\nsecp256k1", "III-F\nalt_bn128"])
+      axs[i].yaxis.set_major_formatter(ticker.FormatStrFormatter('$%.2f'))
+      axs[i].legend()
+
+#ax.set_ylabel("Estimated price in USD")
+#ax.set_xticks([r + bar_width + (0 * bar_width) for r in range(len(create))], ["ETH", "MT", "Semaphore", "PSS\nsecp256k1", "PSS\nalt_bn128"])
+
+#ax = axs[1]
+#ax.set_xticks([r + bar_width + (0 * bar_width) for r in range(len(create))], ["ETH", "MT", "Semaphore", "PSS\nsecp256k1", "PSS\nalt_bn128"])
 
 #plt.ylabel("Estimated price in USD (log)")
 #plt.xticks([r + bar_width for r in range(len(create))], ["Eth. Addr.", "ZoKrates", "Semaphore", "PSS\nsecp256k1", "PSS\nBn128"])
@@ -122,13 +147,12 @@ ax.set_xticks([r + bar_width + (1.5 * bar_width) for r in range(len(create))], [
 #ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
 
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
+#ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y),0)))).format(y)))
 
 #ax.minorticks_off()
 #ax.figure.set_layout_engine('tight')
 #plt.figure(constrained_layout=True)
 
-fig.tight_layout()
+#fig.tight_layout()
 
-plt.legend()
 plt.show()
